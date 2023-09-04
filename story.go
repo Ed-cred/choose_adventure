039eefdac122d47e128b680cc0a34e7901f84c73
Ed-cred <edwardandrew63@gmail.com>
2023-09-04 12:50:12 +0300

    Rendered story paths and added custom templates

--- story.go
+++ story.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"text/template"
 )
 
@@ -21,6 +22,7 @@ type Option struct {
 
 type handler struct {
 	story map[string]Chapter
+	tmpl     *template.Template
 }
 
 var defaultHandlerTmpl = `
@@ -32,15 +34,57 @@ var defaultHandlerTmpl = `
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
-		<h1>{{.Title}}</h1>
-		{{range .Paragraphs}}
-			<p>{{.}}</p>
-		{{end}}
-		<ul>
-			{{range .Options}}
-				<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+		<section class="page">
+			<h1>{{.Title}}</h1>
+			{{range .Paragraphs}}
+				<p>{{.}}</p>
 			{{end}}
-		</ul>
+			<ul>
+				{{range .Options}}
+					<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+				{{end}}
+			</ul>
+		</section>
+		<style>
+		body {
+		  font-family: helvetica, arial;
+		}
+		h1 {
+		  text-align:center;
+		  position:relative;
+		}
+		.page {
+		  width: 80%;
+		  max-width: 500px;
+		  margin: auto;
+		  margin-top: 40px;
+		  margin-bottom: 40px;
+		  padding: 80px;
+		  background: #FECEA8;
+		  border: 1px solid #eee;
+		  box-shadow: 0 10px 6px -6px #777;
+		}
+		ul {
+		  border-top: 1px dotted #ccc;
+		  padding: 10px 0 0 0;
+		  -webkit-padding-start: 0;
+		}
+		li {
+		  padding-top: 10px;
+		}
+		a,
+		a:visited {
+		  text-decoration: none;
+		  color: #6295b5;
+		}
+		a:active,
+		a:hover {
+		  color: #7792a2;
+		}
+		p {
+		  text-indent: 1em;
+		}
+	  </style>
 	</body>
 </html>
 `
@@ -50,15 +94,28 @@ func init() {
 	tmpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
 }
 
-func NewHandler(s map[string]Chapter) http.Handler {
-	return handler{story: s}
+func NewHandler(s map[string]Chapter, t *template.Template) http.Handler {
+	if t == nil {
+		t = tmpl
+	}
+	return handler{story: s, tmpl: t}
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := tmpl.Execute(w, h.story["intro"])
-	if err != nil {
-		log.Fatal("could not execute template", err)
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
 	}
+	path = path[1:] // gets rid of the slash
+	if chapter, ok := h.story[path]; ok {
+		err := tmpl.Execute(w, chapter)
+		if err != nil {
+			log.Fatal("could not execute template", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "Chapter not found.", http.StatusNotFound)
 }
 
 func JsonStoryFromFile(file *os.File) (map[string]Chapter, error) {
